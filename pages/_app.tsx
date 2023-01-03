e031f9cfb8ef07ede34779fef6d41ef4612538dd
Ícar N. S <icar.nin@protonmail.com>
2023-01-03 21:51:56 +0100

    nextjs 13 + montserrat font (rosa d'abril, morena de la serra)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
 import "../styles/global.css";
 
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
