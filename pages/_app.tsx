3998d156c60449a6a6aa1fe909ab912a414ac753
Ícar N. S <icar.nin@protonmail.com>
2023-01-13 00:13:20 +0100

    zilla slab font instead of monospace

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import { AppProps } from "next/app";
-import { Montserrat } from "@next/font/google";
+import { Montserrat, Zilla_Slab } from "@next/font/google";
 import "../styles/global.css";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
+const zillaSlab = Zilla_Slab({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-zilla-slab",
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${zillaSlab.variable}`}>
       <style jsx global>{`
         html {
           font-family: ${montserrat.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: { slab: ["var(--font-zilla-slab)"] },
     extend: {
       colors: {},
     },
