74e1d22d55fc917522160625543af38dba91d285
Ícar N. S <icar.nin@protonmail.com>
2023-01-13 00:15:13 +0100

    heart icon to make it thinner

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="border-t border-gray-300 mt-10 pt-2.5">
-      <p className="font-thin text-xs float-right">
-        Amb &#9825; des de Catalunya i Mallorca
-      </p>
+      <div className="font-thin text-xs float-right inline-block">
+        Amb{" "}
+        <div className="relative w-2 h-2 inline-block">
+          <Image
+            src="/img/heart-thin.svg"
+            alt="heart"
+            fill={true}
+            className="m-auto"
+          />
+        </div>{" "}
+        des de Catalunya i Mallorca
+      </div>
     </footer>
   );
 }
