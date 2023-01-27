7340a1b7e0a4506f388656ea5aa0829cc9cbcf8b
Ícar N. S <icar.nin@protonmail.com>
2023-01-27 20:49:02 +0100

    bottom footer margin

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-300 mt-10 pt-2.5">
+    <footer className="border-t border-gray-300 mt-10 mb-3 pt-2.5">
       <div className="font-thin text-xs text-center w-fit mx-auto">
         Amb{" "}
         <div className="relative w-2 h-2 inline-block">
