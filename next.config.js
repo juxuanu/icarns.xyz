1f6f254087ec6ef8a128b0b9105ea89529526af9
Ícar N. S. <icar.nin@protonmail.com>
2023-09-20 21:32:59 +0200

    housekeeping

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
